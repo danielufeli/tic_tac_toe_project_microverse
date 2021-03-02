057531fdeaec4e9d25ccc526f18b5472246f4fd4
Daniel UFeli <danielufeli@yahoo.coom>
2021-03-02 16:59:39 +0000

    Add Player Class

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,14 @@
 #!/usr/bin/env ruby
-puts 'Hello World'
+require_relative '../lib/player'
 
+def display_name
+  puts 'Please Enter Player1 Name'
+  player1_prompt = gets.chomp
+  puts 'Please Enter Player 2 Name'
+  player2_prompt = gets.chomp
+  player1 = Player.new(player1_prompt, 'x')
+  player2 = Player.new(player2_prompt, 'o')
+  puts "Welcome #{player1.name} and #{player2.name} to tic tac toe"
+end
 
+display_name
---
+++ lib/player.rb
@@ -0,0 +1,8 @@
+class Player
+  attr_accessor :name, :icon
+
+  def initialize(name, icon)
+    @name = name
+    @icon = icon
+  end
+end
