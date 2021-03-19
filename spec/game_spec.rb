ea92c4eeb51f69afe05b5af43e47a9d77163eadf
Promise Johnson <chiemelapromise30@gmail.com>
2021-03-19 20:13:22 +0000

    Removed the inline comments from my code, users cant enter empty name or empty icon as stated by the previous TSE.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,7 +7,6 @@ describe Game do
 
   describe '#valid_move' do
     it 'should return false if a number is not between 1 and 9.' do
-      # game_class.board.board_cell = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']
       expect(game_class.valid_move(8)).to be true
     end
 
@@ -20,7 +19,6 @@ describe Game do
   end
   describe '#game_ended' do
     it 'should return true if the play count is 9. (a draw)' do
-      #   game_class.board.board_cell = %w[x o x o x o x o x]
       game_class.board.board_cell = %w[x o x o x o x o x]
       expect(game_class.game_ended(9)).to be true
     end
